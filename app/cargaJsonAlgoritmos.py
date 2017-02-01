321e53cc9c23e7ba7c38d2f31e8853c56624bcfd
alu0100505078 <alu0100505078@ull.edu.es>
2017-02-01 11:35:45 +0000

    creada carpeta config y empezando con el jquery de los inputfiles

--- app/cargaJsonAlgoritmos.py
+++ app/cargaJsonAlgoritmos.py
@@ -5,15 +5,13 @@ import os
 #cargar json de algoritmos
 def jsonAlgoritmo(request):
   script_dir = os.path.dirname(__file__)
-  file_path = os.path.join(script_dir, 'algoritmos.json')
+  file_path = os.path.join(script_dir, 'config/algoritmos.json')
   with open(file_path) as fi:
     data = json.load(fi)
     parseJson = json.dumps(data, indent=4,sort_keys=True)
-    print parseJson
   outputAlgoritmo=[]  
   for item in range(len(data['algoritmo'])):
    #print 'Current item :', str(data['test'][item]['value']) 
     tipoAlgoritmo= str(data['algoritmo'][item]['value'])
-    print tipoAlgoritmo
     outputAlgoritmo.append(tipoAlgoritmo) 
   return {'algoritmo':outputAlgoritmo}
--- app/cargaJsonTest.py
+++ app/cargaJsonTest.py
@@ -3,7 +3,7 @@ import json
 import os
 def jsonTest(request):
   script_dir = os.path.dirname(__file__)
-  file_path = os.path.join(script_dir, 'tests.json')
+  file_path = os.path.join(script_dir, 'config/tests.json')
   with open(file_path) as fi:
     data = json.load(fi)
     parseJson = json.dumps(data, indent=4,sort_keys=True)

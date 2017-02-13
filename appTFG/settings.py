cd21ed184fd7a5be899c578a727a40f8e2e05579
alu0100505078 <alu0100505078@ull.edu.es>
2017-02-13 12:00:12 +0000

    añadido en settings para cargar ficheros estaticos

--- appTFG/settings.py
+++ appTFG/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -64,7 +65,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'app.cargaJsonTest.jsonTest',
-                'app.cargaJsonAlgoritmos.jsonAlgoritmo',
+                #'app.cargaJsonAlgoritmos.jsonAlgoritmo',
             ],
         },
     },
@@ -101,3 +102,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'app/static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/staticfiles')

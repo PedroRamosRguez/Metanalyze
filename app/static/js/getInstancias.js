c425acd156e68ca5dd1b767ade89e271565f991f
alu0100505078 <alu0100505078@ull.edu.es>
2017-02-21 12:29:02 +0000

    Añadido script para obtener las diferentes instancias introducidas

---
+++ app/static/js/getInstancias.js
@@ -0,0 +1,14 @@
+/*Script para guardar en un array el conjunto de valores de las diferentes instancias introducidas...*/
+
+const instancias = []
+getInstancias = () => {
+  let ninstancias = $('#nInstancia').val()
+  //let instancia = introduceInstancias()
+  let i =1;
+  while (i <= ninstancias){
+    instancias.push($(`#instancia${i}`).val())
+    i+=1;
+  }
+  console.log(instancias)
+  return(instancias)
+}

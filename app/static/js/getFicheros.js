24f97f209b31626da2d8d39dce29256e726fab12
alu0100505078 <alu0100505078@ull.edu.es>
2017-02-21 10:37:35 +0000

    Añadido script para obtener el conjunto de ficheros insertados por el usuario

---
+++ app/static/js/getFicheros.js
@@ -0,0 +1,35 @@
+const ficheros = []
+
+  const fileInput = $("#fichero")
+  $("#introduceAlgoritmo").click(() =>{
+  let input = fileInput.get(0);
+    // Create a reader object
+    let reader = new FileReader();
+    if (input.files.length) {
+        let textFile = input.files[0];
+        // Read the file
+        reader.readAsText(textFile);
+        // When it's loaded, process it
+        $(reader).on('load', processFile);
+    } else {
+        alert('Please upload a file before continuing')
+    }
+    ficheros.push(reader)
+    console.log(ficheros)
+    $("#introduceAlgoritmo").hide();
+    $("#ficheroPruebas").hide();
+  });
+  console.log('probando fuera...')
+
+function processFile(e) {
+    let file = e.target.result,
+        results;
+    if (file && file.length) {
+        //results = file.split("\n");
+        console.log(results)
+    }
+}
+
+getFicheros = () =>{
+  return ficheros
+}

5685b31d7288f0b621d916a5754401e138555bda
alu0100505078 <alu0100505078@ull.edu.es>
2017-03-07 18:41:25 +0000

    eliminando lo de instancias del fichero checkData.js (ya no va lo de instancias)

--- app/static/js/checkData.js
+++ app/static/js/checkData.js
@@ -1,22 +1,5 @@
 /*Script para comprobar que el numero de instancias es de 1-99 y muestra un mensaje tipo span 
 mostrando un error...*/
-
-/*compruebaInstancia = () => {
-  let instancia = document.getElementById('nInstancia').value;
-  console.log(instancia);
-  if(instancia.match(/^[1-9][0-9]{0,1}$/)){
-    $('#spanErrorInstancia').css('display', 'none');
-    $('#nInstancia').css('color','inherit');
-    console.log('acepto...');
-    $('#introduceInstancia').show();
-  }else{
-    console.log('no acepto');
-    $('#nInstancia').css('color','#FF0000');
-    $('#spanErrorInstancia').css('display', 'inherit');
-    $('#introduceInstancia').hide();
-  }
-}
-*/
 checkAlgorithm = () => {
   let nAlgorithms = document.getElementById('nAlgorithms').value;
   console.log(nAlgorithms);

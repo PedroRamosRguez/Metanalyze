18bb35c5792cd723114aef82d2dedc3347f236f7
alu0100505078 <alu0100505078@ull.edu.es>
2017-03-14 17:08:05 +0000

    Añadido el modelo para usarlo con el formulario de django

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Algorithms(models.Model):
+  nAlgorithms = models.CharField(max_length=100)
+  algorithm = models.CharField(max_length=100)
+  file = models.FileField()
+  like = models.CharField(max_length=100)
+  nObjetives = models.CharField(max_length=100)
+  nExecutions = models.CharField(max_length=100)
+  step = models.CharField(max_length=100)
+  stopCondition = models.CharField(max_length=100)

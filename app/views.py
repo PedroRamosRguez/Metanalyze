952ffd8ef3b68a3ed61e3ce87db25e2a40cac02c
alu0100505078 <alu0100505078@ull.edu.es>
2017-03-16 00:50:32 +0000

    arreglada la vista para guardar en media los archivos que se suben

--- app/views.py
+++ app/views.py
@@ -2,9 +2,10 @@
 # Create your views here.
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
-import json
+import json,os
 from .forms import AlgorithmForm
 from .models import Algorithms
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #from .forms import ContactForm
 #en el index carga el json.. mirar para hacerlo desde funcion y tal...(seria facil solo seria importar output y listo)
 def index(request):
@@ -16,20 +17,22 @@ def pruebita(request):
 def pruebatemplate(request):
   if request.method == 'GET':
     print 'es un get de pruebatemplate...'
-    print (request.GET.get('nejecuciones',None))
+    #print (request.GET.get('nejecuciones',None))
     #print (request.GET.get())
   else:
-    if request.is_ajax:
-      print ('es un post de pruebatemplate...')
-      print (request.POST)
-      print (request.POST.getlist('parametros'))
-      #alg = request.POST.getlist('csrfmiddlewaretoken')
-    '''alg = request.POST.getlist('algoritmo[]')
-    #bucle para mostrar los algoritmos que se introdujeron...
-    for i in alg:
-      print ('algoritmo:')
-      print i 
-    print ('es un post...')'''
-    #print (request.POST.get('nejecuciones'))
+    #print (form.fileName)
+    print ('es un post de pruebatemplate...')
+    print (request.POST)
+    print (request.FILES)
+    print (request.FILES.getlist('file'))
+    #metodo para recorrer los archivos subidos y guardarlos en la carpeta media
+    #este metodo y sus bucles son directamente de la documentacion
+    for count, x in enumerate(request.FILES.getlist('file')):
+      def process(f):
+        with open(os.path.join(BASE_DIR, 'media/')+ str(f),'wb+')as destination:
+          for chunk in f.chunks():
+            destination.write(chunk)
+      process(x)
+    return HttpResponse('archivos subidos...')
   return render(request,'app/pruebatemplate.html')
 

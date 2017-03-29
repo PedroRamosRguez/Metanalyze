80036f005db3c5d3edf053c3bc92ac5b6174421d
alu0100505078 <alu0100505078@ull.edu.es>
2017-03-29 18:22:39 +0100

    ya se guardan los valores al form y is_valid devuelve true

--- app/views.py
+++ app/views.py
@@ -1,14 +1,11 @@
 
 # Create your views here.
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render
-from pprint import pprint
 import json,os,ast
 from .forms import AlgorithmForm
 from .models import Algorithms,Configuration
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#from .forms import ContactForm
-#en el index carga el json.. mirar para hacerlo desde funcion y tal...(seria facil solo seria importar output y listo)
 def index(request):
   form = AlgorithmForm()
   return render(request,'app/index.html',{'form': form})
@@ -21,14 +18,17 @@ def pruebatemplate(request):
     #print (request.GET.get('nejecuciones',None))
     #print (request.GET.get())
   else:
-    #print (form.fileName)
     print ('es un post de pruebatemplate...')
-    print request.POST.get('algoritmos')
-    #print type(request.POST.get('algoritmos'))
-    #print (request.POST['algoritmos'][0])
-    #print (request.POST)
+    print request.POST
+    form = AlgorithmForm(request.POST)
+    #convierte a array de diccionarios los valores que se obtienen.
+    dictAlgorithms= ast.literal_eval(request.POST.get('algorithms'))
+    if form.is_valid:
+      print('formulario valido procedo a introducir datos en modelo')
+    else:
+      print('estoy en el else...')
+      #mostrar algun error o algo..
     print (request.FILES)
-    #los prints van aqui...
     #hacer un try para que en caso de que no llegue file no haga nada...
     for count, x in enumerate(request.FILES.getlist('file')):
       def process(f):
@@ -36,8 +36,7 @@ def pruebatemplate(request):
           for chunk in f.chunks():
             destination.write(chunk)
       process(x)
-    #print ('estoy antes de configuration')
-    #print(configuration)
+
     #para introducir el test o los tests que se realicen, se debe hacer un json.dumps()
     #configuration.nObjetives = request.POST.getlist('')
     return HttpResponse('archivos subidos...')

71d15cd99fe0e583ef9e05cc1a0d1626750fe3c8
alu0100505078 <alu0100505078@ull.edu.es>
2017-03-30 12:34:28 +0100

    añadida una coma por si se instalan mas aplicaciones en un futuro

--- appTFG/settings.py
+++ appTFG/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
 )
 MEDIA_ROOT = os.path.join(BASE_DIR,'app/media')
 MEDIA_URL = '/media/'

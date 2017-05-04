6f7f4983abb868a38fe448f4a3b10cf9cc376b3b
alu0100505078 <alu0100505078@ull.edu.es>
2017-05-04 19:00:12 +0100

    añadida foreignkey a configuration ya que varios algoritmos tendran una misma configuracion

--- app/models.py
+++ app/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 # Create your models here.
-# Mirar el paquete jsonfield para la insercion de valores en el modelo tipo json
-class Algorithms(models.Model):
-  
-  algorithm = models.CharField(max_length = 100)   #Nombre del algoritmo
-  idAlgorithm = models.CharField(max_length = 100) #Guarda el identificador que se le de al algoritmo. Ej: NSGA2_90_6_5
-  #file = models.FileField()                        #Fichero adjunto a cada algoritmo insertado
-  fileName = models.CharField(max_length = 100)    #Nombre del fichero adjunto
-  nVariablesAlgorithm = models.CharField(max_length = 100) # Numero de variables que posee el algoritmo a analizar.
-  
 class Configuration(models.Model):
   nAlgorithms = models.CharField(max_length = 100) #Numero de algoritmos
   test = models.CharField(max_length = 100)        #Almacena los tests estadisticos que se realizaran
@@ -20,3 +11,12 @@ class Configuration(models.Model):
   dataOutput = models.CharField(max_length = 100)   #Formato de salida de datos despues de realizar el analisis(grafico,tabla o ambas)
   bound = models.CharField(max_length = 100)        #Guardara la opcion de calcular en la metrica: minimo, media o maximo
   metric = models.CharField(max_length = 100)       # Guarda la metrica o metricas que se analizaran
+# Mirar el paquete jsonfield para la insercion de valores en el modelo tipo json
+class Algorithms(models.Model):
+  
+  algorithm = models.CharField(max_length = 100)   #Nombre del algoritmo
+  idAlgorithm = models.CharField(max_length = 100) #Guarda el identificador que se le de al algoritmo. Ej: NSGA2_90_6_5
+  #file = models.FileField()                        #Fichero adjunto a cada algoritmo insertado
+  fileName = models.CharField(max_length = 100)    #Nombre del fichero adjunto
+  nVariablesAlgorithm = models.CharField(max_length = 100) # Numero de variables que posee el algoritmo a analizar.
+  configuration = models.ForeignKey(Configuration,on_delete=models.CASCADE)

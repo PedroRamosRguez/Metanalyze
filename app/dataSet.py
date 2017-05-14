012912479f0739f2557a643ee904686fe706b3fe
alu0100505078 <alu0100505078@ull.edu.es>
2017-05-14 04:05:55 +0100

    añadido la creacion de dataset del minavgmax chart en fichero dataset

--- app/dataSet.py
+++ app/dataSet.py
@@ -9,5 +9,28 @@ def dataset(data):
 				#print iii
 				#print vvv
 				listValues.append({'x':str(iii),'y':str(vvv)})
+				listValues.sort(key=lambda x:(int(x['x'])))
 		data_scatter.append(listValues)
 	return data_scatter
+
+def datasetMinAvgMax(data):
+	dataList=[]
+	for i,v in enumerate(data):
+	    #print i
+	    #print v
+	    print v.keys()
+        listValues = []
+        for ii,vv in sorted(v.iteritems()):
+            #print ii
+            #print vv
+            print 'esto es ii'
+            print ii
+            listBound = []
+            for iii,vvv in sorted(vv.iteritems()):
+                #print iii
+                #print vvv
+                listBound.append({'x':str(iii),'y':str(vvv)})
+                listBound.sort(key=lambda x:(int(x['x'])))
+            listValues.append(listBound)
+        dataList.append(listValues)
+	return dataList

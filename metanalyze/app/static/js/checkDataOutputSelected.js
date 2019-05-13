429994b88e6ea57c661d82d223470aa40350ceb0
PedroRamosRguez <p.ramos1990@gmail.com>
2019-05-13 21:55:52 +0100

    modificado por cambios en materialize el acceder al valor del radio button en jquery

--- metanalyze/app/static/js/checkDataOutputSelected.js
+++ metanalyze/app/static/js/checkDataOutputSelected.js
@@ -1,9 +1,13 @@
 $('input[name=dataOutput]').change( (e) => {
-	if($('input[name=dataOutput]:checked').val() =='plot'){
+	console.log($('input[type=  radio][name=dataOutput]:checked').next('span').text().trim());
+	if($('input[name=dataOutput]:checked').next().text().trim() === 'Plot'){
+		console.log('plot elegido')
 		$('#xAxisChart').show();
-	}else if($('input[name=dataOutput]:checked').val() =='table-plot'){
+	}else if($('input[name=dataOutput]:checked').next().text().trim() === 'Table and Plot'){
+		console.log('table-plot elegido')
 		$('#xAxisChart').show();
 	}else{
+		console.log('table elegido')
 		$('#xAxisChart').hide();
 	}
 })

0ca7d88417345de281b51932994e99546a74ceac
PedroRamosRguez <p.ramos1990@gmail.com>
2019-05-17 12:54:50 +0100

    añadida url about

--- metanalyze/metanalyze/urls.py
+++ metanalyze/metanalyze/urls.py
@@ -19,5 +19,6 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index,name='index')
+    path('', views.index,name='index'),
+    path('about/', views.about,name='about')
 ]

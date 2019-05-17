39891332be0b474eb603bae4089a627c140c0ae8
PedroRamosRguez <p.ramos1990@gmail.com>
2019-05-17 11:10:19 +0100

    Actualizado a nueva version de Materialize la seleccion multiple, añadida a formulario metricas y seleccion de limites

--- metanalyze/app/forms.py
+++ metanalyze/app/forms.py
@@ -11,35 +11,50 @@ class AlgorithmForm(forms.Form):
         ('no','No')
     )
     CHOICES_evaluations =(
-        ('Number_of_evaluation','Number of evaluation'),
-        ('Time','Time')
+        ('number_of_evaluation','Number of evaluation'),
+        ('time','Time')
     )
-    nAlgorithms = forms.CharField(widget = forms.TextInput(attrs = {'name':'nAlgorithms','id': 'nAlgorithms',
-        'placeholder': 'Insert the number of algorithms', 'oninput': 'checkAlgorithm()'}),required=True)
-              
-    idAlgorithm = forms.CharField(widget = forms.TextInput(attrs={'id':'idAlgorithm',
-        'placeholder':'Introduce the identificator for this algorithm','oninput':'configureAlgorithm()'}),required=False)
+    CHOICES_metrics = (
+        ('hypervolume','Hypervolume'),
+    )
+    CHOICES_bounds = (
+        ('minimum','Minimum'),
+        ('average','Average'),
+        ('maximum','Maximum'),
+    )
+
+    bounds = forms.MultipleChoiceField(widget = forms.CheckboxSelectMultiple,choices = CHOICES_bounds, required = True)
+
+    data_output = forms.ChoiceField(widget = forms.RadioSelect(attrs = {'class': 'with-gap dataOutput'}),choices = CHOICES_dataOutput, required = True)
+
+    evaluation = forms.ChoiceField(widget = forms.RadioSelect(attrs = {'class': 'with-gap dataOutput'}),choices = CHOICES_evaluations,required = False)
+
+    file_input = forms.FileField(widget = forms.FileInput(attrs = {'id':'file'}),required = False)
     
-    file = forms.FileField(widget=forms.FileInput(attrs = {'id':'file'}),required=False)
+    file_name = forms.CharField(widget = forms.TextInput(attrs = {'class':'file-path validate',
+        'placeholder':'Select file to run the test'}),required = False)
+
+    id_algorithm = forms.CharField(widget = forms.TextInput(attrs = {'id':'idAlgorithm',
+        'placeholder':'Introduce the identificator for this algorithm','oninput':'configureAlgorithm()'}),required = False)
+
+    metrics = forms.MultipleChoiceField(widget = forms.CheckboxSelectMultiple,choices = CHOICES_metrics, required = True)
+
+    n_algorithms = forms.CharField(widget = forms.TextInput(attrs = {'name':'nAlgorithms','id': 'nAlgorithms',
+        'placeholder': 'Insert the number of algorithms', 'oninput': 'checkAlgorithm()'}),required = True)
     
-    fileName = forms.CharField(widget=forms.TextInput(attrs = {'class':'file-path validate',
-        'placeholder':'Select file to run the test'}),required=False)
+    n_executions = forms.CharField(widget = forms.TextInput(attrs = {'id': 'nExecutions','name': 'nExecutions',
+        'placeholder': 'Insert the number of executions done'}),required = True)
 
-    nVariablesAlgorithm = forms.CharField(widget=forms.TextInput(attrs = {'id':'nVariablesAlgorithm',
-        'name':'nVariablesAlgorithm','placeholder':'Introduce the number of variables for this algorithm'}),required=False)
-
-    dataOutput = forms.ChoiceField(widget=forms.RadioSelect(attrs = {'class': 'with-gap dataOutput'}),choices=CHOICES_dataOutput, required=True)
+    n_objectives = forms.CharField(widget = forms.TextInput(attrs = {'id': 'nObjectives','name': 'nObjetives',
+        'placeholder': 'Insert the number of objectives'}),required = True)
     
-    nObjectives = forms.CharField(widget = forms.TextInput(attrs= {'id': 'nObjectives','name': 'nObjetives',
-        'placeholder': 'Insert the number of objectives'}),required=True)
+    n_variables_algorithm = forms.CharField(widget = forms.TextInput(attrs = {'id':'nVariablesAlgorithm',
+        'name':'nVariablesAlgorithm','placeholder':'Introduce the number of variables for this algorithm'}),required = False)
 
-    nExecutions = forms.CharField(widget = forms.TextInput(attrs= {'id': 'nExecutions','name': 'nExecutions',
-        'placeholder': 'Insert the number of executions done'}),required=True)
+    statistic_test = forms.ChoiceField(widget = forms.RadioSelect(attrs = {'class': 'with-gap dataOutput'}),choices = CHOICES_statisticTest, required = True)
 
-    step = forms.CharField(widget = forms.TextInput(attrs= {'id': 'step','name': 'step',
-        'placeholder': 'Insert the number of steps'}),required=True)
+    step = forms.CharField(widget = forms.TextInput(attrs = {'id': 'step','name': 'step',
+        'placeholder': 'Insert the number of steps'}),required = True)
 
-    stopCondition = forms.CharField(widget = forms.TextInput(attrs= {'id': 'stopCondition','name': 'stopCondition',
+    stop_condition = forms.CharField(widget = forms.TextInput(attrs= {'id': 'stopCondition','name': 'stopCondition',
         'placeholder': 'Insert the stop condition'}),required=True)
-    statisticTest = forms.ChoiceField(widget=forms.RadioSelect(attrs = {'class': 'with-gap dataOutput'}),choices=CHOICES_statisticTest, required=True)
-    evaluation = forms.ChoiceField(widget=forms.RadioSelect(attrs = {'class': 'with-gap dataOutput'}),choices=CHOICES_evaluations,required=False)
--- metanalyze/app/static/js/start.js
+++ metanalyze/app/static/js/start.js
@@ -1,11 +1,14 @@
 /*Script que oculta algunos elementos de la web cuando se carga...*/
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('select');
+});
 
  $(document).ready(() =>{
  	/*Función de Jquery de materialize para la carga del multiselect*/
  	// $('select').material_select();
-
+	 $('select').formSelect();
  	/* Función de Jquery para la barra de navegación en pantallas pequeñas*/
-	$('.button-collapse').sidenav();
+	  $('.button-collapse').sidenav();
     $('#introduceAlgoritmo').hide();
     $('#spanErrorInstancia').css('display', 'none');
     $('#spanErrorAlgorithms').css('display', 'none');
